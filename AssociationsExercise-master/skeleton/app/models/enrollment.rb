8c491ec976ef1f5cb2fbfe8305e96affc6be211b
sameerank <skunche@gmail.com>
2016-01-20 13:54:33 -0800

    associations exercise

---
+++ AssociationsExercise-master/skeleton/app/models/enrollment.rb
@@ -0,0 +1,39 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :integer          not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Enrollment < ActiveRecord::Base
+
+  # belongs_to(
+  #   :course,
+  #   :class_name => 'Course',
+  #   :foreign_key => :course_id,
+  #   :primary_key => :id
+  # )
+  #
+  # belongs_to(
+  #   :user,
+  #   :class_name => 'User',
+  #   :foreign_key => :student_id,
+  #   :primary_key => :id
+  # )
+
+  belongs_to :course,
+    foreign_key: :course_id,
+    primary_key: :id,
+    class_name: 'Course'
+
+
+  belongs_to :user,
+    foreign_key: :student_id,
+    primary_key: :id,
+    class_name: 'User'
+
+end

34e90b13867c750782b8242992ec1c35c932fee9
sameerank <skunche@gmail.com>
2016-01-22 18:10:56 -0800

    partially fuctional Active Record

---
+++ lib/03_associatable.rb
@@ -0,0 +1,62 @@
+require_relative '02_searchable'
+require 'active_support/inflector'
+
+# Phase IIIa
+class AssocOptions
+  attr_accessor(
+    :foreign_key,
+    :class_name,
+    :primary_key
+  )
+
+  def model_class
+    @class_name.constantize
+  end
+
+  def table_name
+    model_class.table_name
+  end
+end
+
+class BelongsToOptions < AssocOptions
+  def initialize(name, options = {})
+    default = {foreign_key: "#{name}_id".to_sym,
+      class_name: name.to_s.camelcase ,
+      primary_key: :id}
+    options = default.merge(options)
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+  end
+end
+
+class HasManyOptions < AssocOptions
+  def initialize(name, self_class_name, options = {})
+    default = {foreign_key: "#{self_class_name.downcase}_id".to_sym,
+      class_name: name.to_s.camelcase.singularize ,
+      primary_key: :id}
+    options = default.merge(options)
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
+  end
+end
+
+module Associatable
+  # Phase IIIb
+  def belongs_to(name, options = {})
+    # ...
+  end
+
+  def has_many(name, options = {})
+    # ...
+  end
+
+  def assoc_options
+    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+  end
+end
+
+class SQLObject
+  # Mixin Associatable here...
+end

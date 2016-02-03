2ca78e2c979a3bff1296498cde1d1b9adedea3a6
sameerank <skunche@gmail.com>
2016-02-03 13:54:57 -0800

    Searchable can search and extends into SQLObject

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,18 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+    parse_all(DBConnection.execute(<<-SQL, *params.values))
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    WHERE
+      #{where_line}
+    SQL
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end

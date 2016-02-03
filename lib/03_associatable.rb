725d740db41a6e164e8f1b952d2ecea860699d7c
sameerank <skunche@gmail.com>
2016-02-03 14:37:36 -0800

    Associatable has belongs to association

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,30 +10,47 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    default = {foreign_key: "#{name}_id".to_sym,
+      class_name: name.to_s.camelcase ,
+      primary_key: :id}
+    options = default.merge(options)
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    default = {foreign_key: "#{self_class_name.downcase}_id".to_sym,
+      class_name: name.to_s.camelcase.singularize ,
+      primary_key: :id}
+    options = default.merge(options)
+    @foreign_key = options[:foreign_key]
+    @class_name = options[:class_name]
+    @primary_key = options[:primary_key]
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      key_val = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => key_val).first
+    end
   end
 
   def has_many(name, options = {})
@@ -46,5 +63,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
